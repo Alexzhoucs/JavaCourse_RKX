165f94b802e799857eb0d6b6f7182497cf752102
Alex Zhou <alexzhoucs@gmail.com>
2019-04-02 22:41:23 +0800

    base idea of the program

---
+++ homework1/src/Matrix.java
@@ -0,0 +1,12 @@
+
+public class Matrix
+{
+    public static void main(String args[])
+    {
+
+    }
+    //矩阵： 方法：求行列式、求逆矩阵、求伴随矩阵    构造方法：文件输入 + 检查
+    //main： 输入矩阵、检查并求出阶数、求逆矩阵
+}
+
+//行列式： 方法：求值

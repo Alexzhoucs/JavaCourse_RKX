2ea22cf9c65c317606c93ca2fb8fc0797d413bce
Alex Zhou <alexzhoucs@gmail.com>
2019-04-28 18:27:57 +0800

    temp

--- homework2/src/zzw/Freight.java
+++ homework2/src/zzw/Freight.java
@@ -5,6 +5,14 @@ public class Freight
 	private int length;
 
 	private class Car{
+		char cargo1 = '0';
+		char cargo2 = '0';
+		char cargo3 = '0';
+		char cargo4 = '0';
+
+		int weight = 0;
+
+		Car next = null;
 
 	}
 

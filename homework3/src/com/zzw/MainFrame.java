c8811751a2444779550a2f2dbb211ea21248c545
Alex Zhou <alexzhoucs@gmail.com>
2019-06-15 22:56:02 +0800

    temp on 0615

--- homework3/src/com/zzw/MainFrame.java
+++ homework3/src/com/zzw/MainFrame.java
@@ -1,18 +1,26 @@
 package com.zzw;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MainFrame extends JFrame
 {
 	static private final ImageIcon icon = new ImageIcon("./resource/title_cargo.jpg");
+	static ScrollPane sp;
+
 
 	public MainFrame(String title)
 	{
 		super(title);
-		setBounds(500,200,900,600);
-		setResizable(false);
-		setIconImage(icon.getImage());
-		setVisible(true);
+		this.setBounds(500,200,900,600);
+		this.setResizable(true);
+		this.setIconImage(icon.getImage());
+		this.setVisible(true);
+		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+		sp = new ScrollPane(ScrollPane.SCROLLBARS_AS_NEEDED);
+		//todo: set sp
+
 	}
 
 }

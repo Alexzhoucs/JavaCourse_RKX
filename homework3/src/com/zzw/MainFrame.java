7a3ac5d1f67dfeccbb05b78f7f9364ca7145ea6d
Alex Zhou <alexzhoucs@gmail.com>
2019-06-17 00:25:11 +0800

    temp 0616

--- homework3/src/com/zzw/MainFrame.java
+++ homework3/src/com/zzw/MainFrame.java
@@ -7,22 +7,22 @@ public class MainFrame extends JFrame
 {
 	static private final ImageIcon icon = new ImageIcon("./resource/title_cargo.jpg");
 
-	ScrollPane spl;
-	JPanel jpnl;
-	ScrollPane spr;
+	private ScrollPane spl;
+	private JPanel jpnl;
+	private ScrollPane spr;
 
-	JLabel destination;
-	JLabel mount;
-	JLabel location;
+	private JLabel destination = new JLabel("Destination:");
+	private JLabel mount = new JLabel("Mount         :");
+	private JLabel location = new JLabel("Location    :");
 
-	JTextField jtfDestination;
-	JTextField jtfMount;
-	JTextField jtfLocation;
+	private JTextField jtfDestination;
+	private JTextField jtfMount;
+	private JTextField jtfLocation;
 
-	JButton load;
-	JButton unload;
+	private JButton load;
+	private JButton unload;
 
-	JPanel[] jpl;
+	private JPanel[] jpl;
 
 
 	public MainFrame(String title)
@@ -37,43 +37,51 @@ public class MainFrame extends JFrame
 		load = new JButton("Load");
 		unload = new JButton("Unload");
 
-		destination = new JLabel("Destination:\t");
-		mount = new JLabel("Mount:\t\t");
-		location = new JLabel("Location:\t");
+//		destination = new JLabel("Destination:");
+//		mount = new JLabel("Mount:");
+//		location = new JLabel("Location:");
 
 		jtfDestination = new JTextField("A", 5);
 		jtfMount = new JTextField("5", 5);
 		jtfLocation = new JTextField("A", 5);
 		//todo: delete default text
 
-		jpl = new JPanel[8];
-		for (int i = 0; i < 8; i++)
-			jpl[i] = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+		jpl = new JPanel[9];
+		for (int i = 0; i < 9; i++)
+			jpl[i] = new JPanel(new FlowLayout(FlowLayout.CENTER));
 
 		jpl[1].add(destination);
 		jpl[1].add(jtfDestination);
 		jpl[2].add(mount);
 		jpl[2].add(jtfMount);
-		jpl[3].setLayout(new FlowLayout(FlowLayout.CENTER));
+		//jpl[3].setLayout(new FlowLayout(FlowLayout.CENTER));
 		jpl[3].add(load);
 		jpl[5].add(location);
 		jpl[5].add(jtfLocation);
-		jpl[6].setLayout(new FlowLayout(FlowLayout.CENTER));
+		//jpl[6].setLayout(new FlowLayout(FlowLayout.CENTER));
 		jpl[6].add(unload);
 
 
 		jpnl = new JPanel();
-		jpnl.setLayout(new GridLayout(8, 1));
-		for (int i = 0; i < 8; i++)
+		jpnl.setLayout(new GridLayout(9, 1));
+		for (int i = 0; i < 9; i++)
 			jpnl.add(jpl[i]);
+
+//		JPanel jpnlr = new JPanel();
+//		JLabel jl1 = new JLabel("");
 
 		spl = new ScrollPane(ScrollPane.SCROLLBARS_AS_NEEDED);
 		spl.add(jpnl, BorderLayout.CENTER);
-		spl.setPreferredSize(new Dimension(150, 0));
+		spl.setPreferredSize(new Dimension(200, 0));                //设置WEST区域宽度
 
 		this.add(spl, BorderLayout.WEST);
+		//左侧完成
 
-		this.validate();
+		//todo: right side and listener
+
+		this.validate();            //显示组件
 	}
+
+
 
 }
